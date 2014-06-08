9946572584e54bd7c5a6660db2d80c5894b8109e
BJ Miller <bj@sixfivesoftware.com>
2014-06-08 00:24:07 -0400

    Fix unit tests where "only number" was causing issue.
    For some reason, Bob.swift was in the repo, but not in the
    Xcode project, so I added it into Xcode.
    
    I found a method called "countElements(...)" that will return
    a count, for us we needed length of a string, and in this case
    it returns the length of the string "input". From there, the
    regex we worked on would grab the correct range, and so I
    modified the return statement to see if matches.count != 0 (true),
    or false if 0.
    
    Also added a .playground file so we can see tests in action,
    but also to illustrate different test cases for our functions we
    are writing in our tests.
    
    One last change was that there is a .hasSuffix(...) method
    on a String instance, so I adjusted the isQuestion(input:)
    method to use that, rather than the ternary operator.

--- Bob/Bob.swift
+++ Bob/Bob.swift
@@ -10,46 +10,26 @@ import Foundation
 
 class Bob {
     func hey(input: String) -> String {
-        var response: String
         
-        if isShoutingWithQuestion(input) {
+        // initialize response to the default value, saving an extra line later for fallthrough case
+        var response: String = "Whatever."
+        
+        if isShouting(input) {
             response = "Woah, chill out!"
         } else if isQuestion(input) {
             response = "Sure."
-        } else if isShouting(input) {
-            response = "Woah, chill out!"
-        } else {
-            response = "Whatever."
         }
         
         return response
     }
     
     func isQuestion(input: String) -> Bool {
-        return input.rangeOfString("?") ? true : false
+        return input.hasSuffix("?")
     }
     
     func isShouting(input: String) -> Bool {
-         return (input == input.uppercaseString) &&
-            inputDoesNotContainsNumbers(input)
-    }
-    
-    func isShoutingWithQuestion(input: String) -> Bool {
-        return isQuestion(input) && isShouting(input)
-    }
-    
-    func inputDoesNotContainsNumbers(input: String) -> Bool {
-        var containsNumbers = false
-        let numbers = 0...9
-        
-        for number in numbers {
-            if input.rangeOfString(number.description) {
-                containsNumbers = true
-                break
-            }
-        }
-        
-        return containsNumbers
+         return input == input.uppercaseString &&
+            inputContainsALetter(input)
     }
     
     func inputContainsALetter(input: String) -> Bool {
@@ -57,7 +37,8 @@ class Bob {
         
         let regex = NSRegularExpression(pattern: "[A-Za-z]", options: NSRegularExpressionOptions.CaseInsensitive, error: nil)
         
-        NSRange range = NSMakeRange(0, input.length())
+        let range = NSMakeRange(0, countElements(input))
         let matches = regex.matchesInString(input, options: NSMatchingOptions.ReportCompletion, range: range)
+        return (matches.count != 0) ? true : false
     }
 }
